52ca0b1bb578719841dd38b9009ba8c7240d538e
Brian Hicks <therealcaffeine@gmail.com>
2010-10-18 14:44:49 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Portfolio
+=========
+
+This is a portfolio project. This text will be updated before the show, natch.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfolio::Application.config.secret_token = 'd7bbe61b1be4fa6cd54f6c1440e8f1fddc629225c3d70a04afd2321df29f89003ab38921ea07d4ef0a078450e079d96a918f507716e97f358261fb593df7ef94'
