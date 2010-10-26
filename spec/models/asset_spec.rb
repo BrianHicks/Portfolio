6b92550eb6133669794d07545b258f91e1cf4b15
Brian Hicks <therealcaffeine@gmail.com>
2010-10-26 16:09:03 -0500

    modified automatically generated tests

--- spec/models/asset_spec.rb
+++ spec/models/asset_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe Asset do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:portolio_item) }
 end
--- spec/models/portfolio_item_spec.rb
+++ spec/models/portfolio_item_spec.rb
@@ -2,6 +2,7 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe PortfolioItem do
   it { should belong_to(:user) }
+  it { should have_many(:assets) }
 
   it "should be valid" do
     PortfolioItem.new.should be_valid
